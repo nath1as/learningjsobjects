d39ae31965578f160243c2ccc9186aaca14d451f
nathias <n@th1.as>
2018-07-17 12:11:16 +0200

    getMonth

---
+++ getMonth.js
@@ -0,0 +1,28 @@
+var today = new Date();
+var daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function dateSuffix(date) {
+  var suffix = 'th';
+  var dateString = String(date);
+  var digit;
+
+  if (dateString.length === 1 ) {
+    digit = dateString[0];
+  } else if (dateString[0] !== '1') {
+    digit = dateString[1];
+  }
+
+  if (digit === '1') {
+    suffix = 'st';
+  } else if (digit === '2') {
+    suffix = 'nd';
+  } else if (digit === '3') {
+    suffix = 'rd';
+  }
+
+  return String(date) + suffix;
+}
+
+console.log("Today's date is " + daysOfWeek[today.getDay()] + ', ' +
+                                 String(today.getMonth()) + ' ' +
+                                 dateSuffix(today.getDate()));
